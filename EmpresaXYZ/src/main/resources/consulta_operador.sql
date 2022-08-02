5b9991c6a51f2d566523908d14dfd65255443f4b
Francisco Gallardo <francisco.gallardo@infotec.mx>
2022-08-01 19:04:21 -0500

    se sube pequeña api

---
+++ EmpresaXYZ/src/main/resources/consulta_operador.sql
@@ -0,0 +1,12 @@
+
+
+create database examen_asteci;
+
+create table operador
+
+select * from incidencia;
+select * from operador;
+
+select id FROM incidencia;
+select dia FROM incidencia;
+select id_operador FROM incidencia;
