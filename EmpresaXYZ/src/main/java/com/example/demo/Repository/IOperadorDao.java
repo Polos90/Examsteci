8eb777618ae100f8b07421ef142a4e8c490d4bff
Polos90 <49696812+Polos90@users.noreply.github.com>
2022-08-03 23:04:24 -0500

    Delete EmpresaXYZ directory

--- EmpresaXYZ/src/main/java/com/example/demo/Repository/IOperadorDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repository;
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.modelo.Operador;
-public interface IOperadorDao extends CrudRepository<Operador, Long> {
-
-}
